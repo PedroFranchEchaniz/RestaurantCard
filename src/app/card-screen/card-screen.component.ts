d2ce5dc8c8dc545dca0198ba5d07c1b04d3e2c47
Pedro Franch Echaniz <pedrofe.av@gmail.com>
2023-10-12 20:07:24 +0200

    html card

---
+++ src/app/card-screen/card-screen.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-screen',
+  templateUrl: './card-screen.component.html',
+  styleUrls: ['./card-screen.component.css']
+})
+export class CardScreenComponent {
+
+}
